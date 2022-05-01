4913f5f83560c4174ba6454c41e43002e47586ba
AbdMamun1999 <mamunrox@gmail.com>
2022-05-01 13:40:59 +0600

    added inventory section on home page

--- src/Components/Inventories/Inventories.js
+++ src/Components/Inventories/Inventories.js
@@ -1,21 +1,31 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Inventory from '../Inventory/Inventory';
+import './inventories.css'
 
 const Inventories = () => {
-    const [inventories,setInventories] = useState([])
+    const [inventories, setInventories] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('http://localhost:5000/inventory')
-        .then(response =>{
-            const {data} = response
-            setInventories(data)
-        })
-    },[])
+            .then(response => {
+                const { data } = response
+                setInventories(data)
+            })
+    }, [])
 
     return (
         <div>
             <h1 className='text-center'>Inventory</h1>
-            
+            <div className='inventories-container'>
+                {
+                    inventories.map(inventory => <Inventory
+                        key={inventory._id}
+                        inventory={inventory}
+                    ></Inventory>)
+                }
+            </div>
+
         </div>
     );
 };
---
+++ src/Components/Inventory/Inventory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Inventory = (props) => {
+    const {price,name,quantity} = props.inventory
+    return (
+        <div className=' border'>
+            <h2>{name}</h2>
+            <p>Price:{price}</p>
+            <p>Quantity:{quantity}</p>
+        </div>
+    );
+};
+
+export default Inventory;
