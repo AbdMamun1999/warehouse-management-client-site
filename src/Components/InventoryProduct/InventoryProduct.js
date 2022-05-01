17ea4dde253d8acf8c3660c186faf7ddff9574e2
AbdMamun1999 <mamunrox@gmail.com>
2022-05-01 21:52:06 +0600

    fetch all data on manage Inventory page with axios and added route for manageInventory page and Inventory Page componet

---
+++ src/Components/InventoryProduct/InventoryProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryProduct = () => {
+    return (
+        <div>
+            <h3>Manage Inventory Products</h3>
+        </div>
+    );
+};
+
+export default InventoryProduct;
--- src/Components/ManageInventory/ManageInventory.js
+++ src/Components/ManageInventory/ManageInventory.js
@@ -1,10 +1,21 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const ManageInventory = () => {
-
+    const [inventory,setInventory] = useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:5000/inventories')
+        .then(respose => {
+            console.log(respose)
+            const {data} = respose
+            console.log(data)
+        })
+    },[])
     return (
         <div>
             <h3>Manage Inventory</h3>
+            {
+            }
         </div>
     );
 };
