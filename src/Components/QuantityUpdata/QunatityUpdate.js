0b13f87c87d52a4789d19d24009d516391e1b0e0
AbdMamun1999 <mamunrox@gmail.com>
2022-05-01 20:49:15 +0600

    added decrease button  on /inventory/:id page

--- src/Components/QuantityUpdata/QunatityUpdate.js
+++ src/Components/QuantityUpdata/QunatityUpdate.js
@@ -15,27 +15,49 @@ const QunatityUpdate = () => {
             })
     }, [inventory])
 
+
+    const inputValue = event => {
+        const quantity = parseInt(event.target.value);
+        setQuantity(quantity)
+    }
+
     const quantityUpdate = async event => {
         event.preventDefault()
-        const quantity = parseInt(event.target.quantity.value);
+
         const previousQuantity = inventory.quantity
         if (quantity > 0) {
             const currentQuantity = quantity + previousQuantity;
             console.log(currentQuantity)
             const url = `http://localhost:5000/inventory/${inventoryId}`
 
-            const { data } = await axios.put(url, {currentQuantity })
+            const { data } = await axios.put(url, { currentQuantity })
         }
 
     }
+
+
+    const decreaseQuantity = async event => {
+        event.preventDefault()
+
+        const previousQuantity = inventory.quantity
+        if (quantity > 0) {
+            const currentQuantity = previousQuantity - quantity;
+            console.log(currentQuantity)
+            const url = `http://localhost:5000/inventory/${inventoryId}`
+
+            const { data } = await axios.put(url, { currentQuantity })
+        }
+    }
+
     return (
         <div>
             <h2>Quantity Update page :{inventoryId}</h2>
-            <form onSubmit={quantityUpdate}>
-                <label htmlFor="quantity">Quantity For The Product :{inventory.quantity}</label><br /><br />
-                <input type="text" placeholder='Quantity' name='quantity' /> <br /><br />
-                <button>Quantity Update</button>
-            </form>
+            {/* <form > */}
+            <label htmlFor="quantity">Quantity For The Product :{inventory.quantity}</label><br /><br />
+            <input type="text" onBlur={inputValue} placeholder='Quantity' name='quantity' /> <br /><br />
+            <button onClick={quantityUpdate}>Added Quantity</button>
+            <button onClick={decreaseQuantity}>Decrease Quantity</button>
+            {/* </form> */}
         </div>
     );
 };
