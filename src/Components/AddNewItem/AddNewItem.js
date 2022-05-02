f6f95cf8184df46307935149dc27ff9b8482a473
AbdMamun1999 <mamunrox@gmail.com>
2022-05-02 12:06:28 +0600

    added add item and its functionality

--- src/Components/AddNewItem/AddNewItem.js
+++ src/Components/AddNewItem/AddNewItem.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import React from 'react';
 
 const AddNewItem = () => {
+    const addItem =async event =>{
+        event.preventDefault()
+        const img = event.target.url.value;
+        const name = event.target.name.value;
+        const url = `http://localhost:5000/inventory`
+        const {data} = await axios.post(url,{img,name})
+    }
     return (
         <div>
             <h1>Add New Item</h1>
+            <form onSubmit={addItem}>
+                <input type="text" name="url" id="" placeholder='Image Url'/>
+                <input type="text" name="name" id="" placeholder='Name'/>
+                <input type="submit" value="Add Item" />
+            </form>
         </div>
     );
 };
