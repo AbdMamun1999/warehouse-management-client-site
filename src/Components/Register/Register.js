65ce7aeeae19761b33179f65dbb0edcfe65aeabd
AbdMamun1999 <mamunrox@gmail.com>
2022-05-02 18:09:05 +0600

    some style add on sign up page

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Register.css'
+import { Link } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+
 
 const Register = () => {
     return (
@@ -13,6 +16,12 @@ const Register = () => {
                     <input type="password" name="confirm-password" id="" placeholder='Confirmation Password' required />
                     <input type="submit" value="Submit" />
                 </form>
+                <div className='d-flex align-items-end justify-content-end mt-3'>
+                    <p className='fw-bold link'>
+                        Have already an account?<span>
+                         <Link className='text-dark' to={'/login'}>Login here</Link></span>
+                    </p>
+                </div>
             </div>
         </div>
     );
