305ddc39eb7310a21ee3670b01fdb16730fe040e
AbdMamun1999 <mamunrox@gmail.com>
2022-05-04 07:33:47 +0600

    style login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Login.css'
+import google from '../../images/social/google.png'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Log in Page</h1>
+        <div className='login'>
+            <div className="login-container">
+                <h2 className='text-start mb-5'>Login</h2>
+                <form>
+                    <input type="email" name="email" id="" placeholder='Email' required />
+                    <input type="password" name='password' placeholder='Password' required />
+                    <input type="submit" value="Login" />
+                </form>
+                <div className='horizental-line'>
+                    <div></div>
+                    <span>Or</span>
+                    <div></div>
+                </div>
+                <div className="social-login">
+                    <div>
+                       <button>
+                           <img src={google} alt="" />
+                       </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
