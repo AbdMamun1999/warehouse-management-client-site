8a0900e70e7aa567cdf4921af0197e5491eead32
AbdMamun1999 <mamunrox@gmail.com>
2022-05-04 14:05:17 +0600

    create user with email and password

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,25 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css'
-import { Link } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 
 const Register = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfoirmPassword] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+    const navigate = useNavigate()
+
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    
+
+    const handleEmail = event => {
+        const email = event.target.value;
+        setEmail(email)
+    }
+
+    const handlePassword = event => {
+        const password = event.target.value;
+        setPassword(password)
+    }
+
+    const hadleConfirmPassword = event => {
+        const confirmPassword = event.target.value;
+        setConfoirmPassword(confirmPassword)
+    }
+
+    if (user) {
+        navigate('/')
+    }
+
+    const handleRegister = async event => {
+        event.preventDefault()
+        if (password === confirmPassword) {
+            createUserWithEmailAndPassword(email, password)
+        } else {
+            setPasswordError('Confirm password do not match')
+        }
+
+    }
+
+    console.log(user)
+
     return (
         <div className='register'>
             <h1>WELCOME TO WAREHOUSE MANAGEMENT</h1>
             <div className='register-container'>
-                <h2>Sign Up</h2>
-                <form>
-                    <input type="email" name="email" id="" placeholder='Email' required />
-                    <input type="password" name="password" id="" placeholder='Password' required />
-                    <input type="password" name="confirm-password" id="" placeholder='Confirmation Password' required />
+                <h2 className='mb-3'>Sign Up</h2>
+                <form onSubmit={handleRegister}>
+                    <input onBlur={handleEmail} type="email" name="email" id="" placeholder='Email' required />
+                    <input onBlur={handlePassword} type="password" name="password" id="" placeholder='Password' required />
+                    <input onBlur={hadleConfirmPassword} type="password" name="confirm-password" id="" placeholder='Confirmation Password' required />
+                    <p className='error-massage'>{passwordError}</p>
                     <input type="submit" value="Submit" />
                 </form>
                 <div className='d-flex align-items-end justify-content-end mt-3'>
                     <p className='fw-bold link'>
                         Have already an account?<span>
-                         <Link className='text-dark' to={'/login'}>Login here</Link></span>
+                            <Link className='text-dark' to={'/login'}>Login here</Link></span>
                     </p>
                 </div>
             </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
-import { getAuth } from "firebase/auth";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyBrqz97bKc6DfsuAy6lrUiR4qhtj2LWp0w",
+  authDomain: "warehouse-management-authentic.firebaseapp.com",
+  projectId: "warehouse-management-authentic",
+  storageBucket: "warehouse-management-authentic.appspot.com",
+  messagingSenderId: "38610421282",
+  appId: "1:38610421282:web:4b9dfdc883ca28bc56ec7b"
 };
 
 // Initialize Firebase
