8d6333a9f3a9e35a8de739b21945f98e6f33dc74
AbdMamun1999 <mamunrox@gmail.com>
2022-05-08 11:44:31 +0600

    added why to choose component on home page

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,12 +2,14 @@ import Banner from '../Banner/Banner';
 import Farmers from '../Farmers/Farmers';
 import Footer from '../Footer/Footer';
 import Inventories from '../Inventories/Inventories';
+import WhyToChooseUs from '../WhyToChooseUs/WhyToChooseUs';
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
             <Inventories></Inventories>
+            <WhyToChooseUs></WhyToChooseUs>
             <Farmers></Farmers>
             <Footer></Footer>
 
---
+++ src/Components/WhyToChooseUs/WhyToChooseUs.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './WhyToChooseUs.css'
+import icon1 from '../../images/icon/icon1.png'
+import icon2 from '../../images/icon/icon2.png'
+import icon3 from '../../images/icon/icon3.png'
+import icon4 from '../../images/icon/icon4.png'
+
+const WhyToChooseUs = () => {
+    return (
+        <div className='why-to-choose-us'>
+            <div className='why-to-choose-top'>
+                <h1 className='farmer-header'>WHY TO CHOOSE US</h1>
+                <p className='why-header-para'>THERE ARE MANY VARIATIONS OF PASSAGES OF LOREM IPSUM AVAILABLE, BUT THE MAJORITY HAVE SUFFERED ALTERATION IN SOME FORM, BY INJECTED HUMOUR.</p>
+            </div>
+            <div className="why-to-choose-bottom">
+                <div className='why-to-choose-bottom-wrapper'>
+                    <div>
+                        <img src={icon1} alt="" />
+                    </div>
+                    <div>
+                        <h3>
+                            100% Organic Products
+                        </h3>
+                        <p>
+                        Organic products reduce public health risks to farm workers, their families, and consumers by minimizing their exposure to toxic and persistent chemicals on the farm and in food, the soil in which they work and play, the air they breathe, and the water they drink. Children are especially vulnerable to pesticides.
+                        </p>
+                    </div>
+                </div>
+                <div className='why-to-choose-bottom-wrapper'>
+                    <div>
+                        <img src={icon2} alt="" />
+                    </div>
+                    <div>
+                        <h3>
+                            Any Time, Anywhere Delivery
+                        </h3>
+                        <p>
+                        Anytime, Anyplace, Anywhere: the new last-mile delivery. We are continuously customizing our offers. Delivering from the store to anywhere within 1 hour.
+                        </p>
+                    </div>
+                </div>
+                <div className='why-to-choose-bottom-wrapper'>
+                    <div>
+                        <img src={icon3} alt="" />
+                    </div>
+                    <div>
+                        <h3>
+                            Keeps Your Family Healthy
+                        </h3>
+                        <p>
+                        Fruit and vegetables should be an important part of your daily diet. They are naturally good and contain vitamins and minerals that can help to keep you healthy. They can also help protect against some diseases.
+                        </p>
+                    </div>
+                </div>
+                <div className='why-to-choose-bottom-wrapper'>
+                    <div>
+                        <img src={icon4} alt="" />
+                    </div>
+                    <div>
+                        <h3>
+                            Clean, Fresh and Safety
+                        </h3>
+                        <p>
+                            Fresh fruits and vegetables are
+                            an important part of a healthful diet. They provide vitamins,
+                            minerals and fiber to help keep
+                            your body healthy
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WhyToChooseUs;
